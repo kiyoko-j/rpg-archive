5877bb7f0f41d180e80fb2ad4d1b1b8f666eb433
Johan Bäckman <johan.backman@tobii.com>
2023-03-14 17:29:10 +0100

    Make config detect contentDirs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,5 +1,22 @@
-import { defineConfig } from 'vitepress';
+import { DefaultTheme, defineConfig } from 'vitepress';
 import { getSidebar } from 'vitepress-plugin-auto-sidebar';
+
+// @ts-ignore-next
+import fs from 'node:fs';
+// @ts-ignore-next
+import path from 'node:path';
+
+const excludeDirs = ['assets', '.vitepress'];
+
+const contentDirs = fs
+  // @ts-ignore-next
+  .readdirSync(path.join(__dirname, '..'), { withFileTypes: true })
+  .filter((file) => file.isDirectory())
+  .map((dir) => dir.name)
+  .filter((dir) => excludeDirs.includes(dir) === false);
+
+console.log('Building for content dirs:');
+console.log(contentDirs);
 
 export default defineConfig({
   title: 'RPG Archive',
@@ -9,10 +26,10 @@ export default defineConfig({
     outline: 'deep',
     sidebar: getSidebar({
       contentRoot: 'docs',
-      contentDirs: ['dying-earth', 'horror-noire'],
+      contentDirs: contentDirs,
       collapsible: true,
       collapsed: false,
-    }),
+    }) as DefaultTheme.Sidebar,
     editLink: {
       pattern: 'https://github.com/jobackman/rpg-archive/edit/main/docs/:path',
       text: 'Edit this page on Github',
